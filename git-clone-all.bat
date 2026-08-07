979f76f79efc81c41c7af77e9fbcd021dd6b4ab0
magicon-top <sontrava@gmail.com>
2026-08-07 17:17:42 +0300

    refactor: update git-clone-all.bat to improve command execution logic

--- git-clone-all.bat
+++ git-clone-all.bat
@@ -1,8 +1,17 @@
 @echo off
-:: gh clone-org magicon-top
 chcp 65001 > nul
+
+:: Переходим в родительскую папку на уровень выше
+cd /d "%~dp0.."
+
 for /f "tokens=*" %%i in ('gh repo list magicon-top --limit 1000 --json name --jq ".[].name"') do (
-    echo Клонируем репозиторий: %%i...
-    gh repo clone magicon-top/%%i %%i
+    :: Проверяем, существует ли уже папка с таким репозиторием
+    if exist "%%i" (
+        echo [Пропущено] Папка "%%i" уже существует.
+    ) else (
+        echo Клонируем репозиторий: %%i...
+        gh repo clone magicon-top/%%i %%i
+    )
 )
+echo --- DONE ---
 pause
